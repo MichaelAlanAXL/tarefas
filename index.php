b30e6cccb53a938f1458fe8b552a740e306dc816
unknown <michael.dtna@gmail.com>
2024-01-19 22:36:36 -0300

    configurando templates do site

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php 
 
-echo "hello world!";
+require_once("vendor/autoload.php");
+
+use \Slim\Slim;
+use \App\Page;
+
+$app = new Slim();
+
+$app->config('debug', true);
+
+//echo "hello world!";
+$app->get('/', function(){
+
+    //$products = Product::listAll();
+
+    $page = new Page();
+
+    $page->setTpl("index");
+
+});
+
+$app->run();
 
  ?>
