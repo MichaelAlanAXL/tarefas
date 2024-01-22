a280aeb30e22447304ea4c2b88baa8294faf3b77
unknown <michael.dtna@gmail.com>
2024-01-22 08:05:02 -0300

    layout done

--- views-cache/index.1b583061e468b04e757af121d221a81c.rtpl.php
+++ views-cache/index.1b583061e468b04e757af121d221a81c.rtpl.php
@@ -1,23 +1,31 @@
 <?php if(!class_exists('Rain\Tpl')){exit;}?>
-<main>
+<main class="container">
 	<h1>Lista de Tarefas</h1>
 
-	<table>
+	<div class="box-header">
+		<a href="/admin/users/create" class="btn btn-success">Cadastrar uma nova tarefa</a>
+    </div>
+
+	<table class="table table-hover">
 		<thead>
 			<tr>
-				<th>Número</th>
-				<th>Título</th>
-				<th>Descrição</th>
-				<th>Status</th>
+				<th scope="col">Número</th>
+				<th scope="col">Título</th>
+				<th scope="col">Descrição</th>
+				<th scope="col">Status</th>
 			</tr>
 		</thead>
 		<tbody>
 			<?php $counter1=-1;  if( isset($dados) && ( is_array($dados) || $dados instanceof Traversable ) && sizeof($dados) ) foreach( $dados as $key1 => $value1 ){ $counter1++; ?>
 				<tr>
-					<th><?php echo htmlspecialchars( $value1["id"], ENT_COMPAT, 'UTF-8', FALSE ); ?></th>
+					<th scope="row"><?php echo htmlspecialchars( $value1["id"], ENT_COMPAT, 'UTF-8', FALSE ); ?></th>
 					<td><?php echo htmlspecialchars( $value1["title"], ENT_COMPAT, 'UTF-8', FALSE ); ?></td>
 					<td><?php echo htmlspecialchars( $value1["description"], ENT_COMPAT, 'UTF-8', FALSE ); ?></td>
-					<td><?php echo htmlspecialchars( $value1["status"], ENT_COMPAT, 'UTF-8', FALSE ); ?></td>					
+					<td><?php echo htmlspecialchars( $value1["status"], ENT_COMPAT, 'UTF-8', FALSE ); ?></td>	
+					<td>
+						<a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit fa-lg"></i>Editar</a>
+						<a href="#" class="btn btn-danger btn-sm"><i class="fa fa-trash fa-lg"></i>Excluir</a>
+					</td>				
 				</tr>				
 			<?php } ?>
 		</tbody>
